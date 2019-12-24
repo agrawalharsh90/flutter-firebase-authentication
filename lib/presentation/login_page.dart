8337bdab5e77b6c9031a989975d8691074fb65d9
Harsh Agrawal <agrawalharsh90@gmail.com>
2019-12-24 14:34:50 +0530

    minor changes

--- lib/presentation/login_page.dart
+++ lib/presentation/login_page.dart
@@ -1,18 +1,88 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_email_auth/presentation/home_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String _email;
+
+  String _password;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Log In"),
+        title: Text("Log IN"),
       ),
       body: Center(
-        child: Text(
-          "Welcome",
-          style: TextStyle(fontSize: 20),
+          child: Container(
+        margin: EdgeInsets.all(30),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                validator: (input) {
+                  if (input.isEmpty) {
+                    return 'Please type an email';
+                  }
+                },
+                onSaved: (input) => _email = input,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                validator: (input) {
+                  if (input.length < 6) {
+                    return 'Please type an valid password';
+                  }
+                },
+                onSaved: (input) => _password = input,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              RaisedButton(
+                onPressed: _login,
+                child: Text('Log IN'),
+              )
+            ],
+          ),
         ),
-      ),
+      )),
     );
+  }
+
+  _login() async {
+    final formState = _formKey.currentState;
+    if (formState.validate()) {
+      formState.save();
+      try {
+        print("auth request");
+        AuthResult authResult = await FirebaseAuth.instance
+            .signInWithEmailAndPassword(email: _email, password: _password);
+        print("result");
+        print(authResult.toString());
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => HomePage()));
+      } catch (e) {
+        print("error");
+        print(e.message);
+      }
+    }
   }
 }
--- lib/presentation/signup_page.dart
+++ lib/presentation/signup_page.dart
@@ -1,18 +1,121 @@
 import 'package:flutter/material.dart';
 
-class SignupPage extends StatelessWidget {
+class SignupPage extends StatefulWidget {
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  String _name;
+
+  String _phone;
+
+  String _email;
+
+  String _password;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Sign Up"),
+        title: Text("Sign UP"),
       ),
       body: Center(
-        child: Text(
-          "Welcome",
-          style: TextStyle(fontSize: 20),
+          child: Form(
+        key: _formKey,
+        child: Container(
+          margin: EdgeInsets.all(30),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                validator: (input) {
+                  if (input.isEmpty) {
+                    return 'Please type Name';
+                  }
+                },
+                onSaved: (input) => _name = input,
+                decoration: InputDecoration(
+                  labelText: 'Name',
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                validator: (input) {
+                  if (input.length < 10) {
+                    return 'Please type an valid phone number';
+                  }
+                },
+                onSaved: (input) => _phone = input,
+                decoration: InputDecoration(
+                  labelText: 'Phone Number',
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                validator: (input) {
+                  if (input.isEmpty) {
+                    return 'Please type an email';
+                  }
+                },
+                onSaved: (input) => _email = input,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                validator: (input) {
+                  if (input.length < 6) {
+                    return 'Please type an valid password';
+                  }
+                },
+                onSaved: (input) => _password = input,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                validator: (input) {
+                  if (input != _password) {
+                    return 'Password mismatch';
+                  }
+                },
+                decoration: InputDecoration(
+                  labelText: 'Confirm Password',
+                ),
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              RaisedButton(
+                onPressed: _signup,
+                child: Text('Sign UP'),
+              )
+            ],
+          ),
         ),
-      ),
+      )),
     );
+  }
+
+  _signup() {
+    final formState = _formKey.currentState;
+    if (formState.validate()) {
+      //firbase inegrate
+      print("form validated");
+    }
   }
 }
